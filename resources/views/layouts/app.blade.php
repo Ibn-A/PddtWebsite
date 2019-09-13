e3dc6dd2fb7c6c6dbb0a958a358587146a1a1ddb
Ibn-A <ibn_ali@yahoo.com>
2019-09-13 12:30:51 +0200

    modif_app.blade_navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Pddt') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,20 +21,31 @@
 </head>
 <body>
     <div id="app">
+        <div>reseaux sociaux</div>
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <div><img src="/img/pddtLogo.png" style="height:70px ;" alt="Logo du Pddt"></div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">{{ __('Qui somme nous ?') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">{{ __('Nos projets') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">{{ __('Nos partenaires') }}</a>
+                    </li>
 
-                    </ul>
+                    </ul> 
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
@@ -46,6 +57,9 @@
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __("s'inscrire") }}</a>
+                                </li>
+                                <li class="nav-item">
+                                <button type="button" class="btn btn-success">Faire un Don</button>
                                 </li>
                             @endif
                         @else

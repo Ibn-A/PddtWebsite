1daa93689dd2da76105998daa9652c3d544dcdfb
Ibn-A <ibn_ali@yahoo.com>
2019-09-17 12:03:52 +0200

    img_caroussel_widthModif

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,21 +11,21 @@
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
-      <img src="https://ps.w.org/wp-carousel-free/assets/banner-1544x500.png?rev=2000936" alt="...">
+      <img style="width:100%;" src="https://ps.w.org/wp-carousel-free/assets/banner-1544x500.png?rev=2000936" alt="...">
         <div class="carousel-caption d-none d-md-block">
           <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="https://ps.w.org/wp-carousel-free/assets/banner-1544x500.png?rev=2000936" alt="...">
+        <img style="width:100%;" src="https://ps.w.org/wp-carousel-free/assets/banner-1544x500.png?rev=2000936" alt="...">
         <div class="carousel-caption d-none d-md-block">
           <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="https://ps.w.org/wp-carousel-free/assets/banner-1544x500.png?rev=2000936" alt="...">
+        <img style="width:100%;" src="https://ps.w.org/wp-carousel-free/assets/banner-1544x500.png?rev=2000936" alt="...">
         <div class="carousel-caption d-none d-md-block">
           <h5>Third slide label</h5>
           <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>

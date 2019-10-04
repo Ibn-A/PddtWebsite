0df0a4ec96a23f0d52e845e8f471ec4cac323cb5
andri10 <andrijp10@gmail.com>
2019-10-04 03:18:14 +0200

    ACCUEIL fini !!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,164 +2,171 @@
 
 @section('content')
 
-<!-- Styles -->
-<link href="{{ asset('css/accueil.css') }}" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('css/accueil.css') }}" rel="stylesheet">
 
-<div class="bd-example">
-  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-      <img src="https://pddt-tchad.org/wp-content/uploads/2016/02/head_01.jpg" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    <header>
+        <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <!-- Slide One - Set the background image for this slide in the line below -->
+                <div class="carousel-item active" style="background-image: url('http://placehold.it/1920x1080')">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2 class="display-4">First Slide</h2>
+                        <p class="lead">This is a description for the first slide.</p>
+                    </div>
+                </div>
+                <!-- Slide Two - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url('http://placehold.it/1920x1080')">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2 class="display-4">Second Slide</h2>
+                        <p class="lead">This is a description for the second slide.</p>
+                    </div>
+                </div>
+                <!-- Slide Three - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url('http://placehold.it/1920x1080')">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2 class="display-4">Third Slide</h2>
+                        <p class="lead">This is a description for the third slide.</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-      </div>
-      <div class="carousel-item">
-        <img src="https://pddt-tchad.org/wp-content/uploads/2016/02/head_01.jpg" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="https://pddt-tchad.org/wp-content/uploads/2016/02/head_01.jpg" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
+    </header>
 
-    <section class="container">
-
+    <section>
+    
         <div class="bloc-1">
-            <div class="article-1">
-                <p>
+            <div class ="container">
+                <div class="article-1">
+                    <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">Lorem</h1>
+                    <hr class="mt-2 mb-4">
+                    <p>
                     Eodem tempore etiam Hymetii praeclarae indolis viri negotium est actitatum, cuius hunc novimus esse textum. 
                     cum Africam pro consule regeret Carthaginiensibus victus inopia iam lassatis, ex horreis Romano.
-                </p>
-            </div>
-
-            <div>
-                <iframe class="video" src="https://www.youtube.com/embed/VFzt6l9V_2o" allowfullscreen></iframe>
+                    </p>
+                </div>
+                <div class="text-center">
+                    <iframe class="video" src="https://www.youtube.com/embed/VFzt6l9V_2o" allowfullscreen></iframe>
+                </div>
             </div>
         </div>
         
         <div class="bloc-2">
-            <h2 class="titre">Nos projets</h2>
-
-            <div class="projet">
-                
-                <div class="box">
-                    <div class="imgBx">
-                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/01/Sensibilisation_une.jpg">
-                    </div>
-                    <div class="content">
-                        <div>
-                            <h2>Image Title</h2>
-                            <p>
+            <div class ="container">
+                <h1 class="font-weight-light text-center">Nos Projets</h1>
+                <div class="projet text-center">
+                    <div class="box">
+                        <div class="imgBx">
+                            <img src="http://placehold.it/275">
+                        </div>
+                        <div class="content">
+                            <div>
+                                <h2>Image Title</h2>
+                                <p>
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur pariatur 
                                 nesciunt adipisci quam molestiae quasi quibusdam accusamus ullam quod ex modi 
                                 repudiandae, provident impedit ab amet culpa rerum aspernatur aut?
-                            </p>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <div class="imgBx">
+                            <img src="http://placehold.it/275">
+                        </div>
+                        <div class="content">
+                            <div>
+                                <h2>Image Title</h2>
+                                <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur pariatur 
+                                nesciunt adipisci quam molestiae quasi quibusdam accusamus ullam quod ex modi 
+                                repudiandae, provident impedit ab amet culpa rerum aspernatur aut?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <div class="imgBx">
+                            <img src="http://placehold.it/275">
+                        </div>
+                        <div class="content">
+                            <div>
+                                <h2>Image Title</h2>
+                                <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur pariatur 
+                                nesciunt adipisci quam molestiae quasi quibusdam accusamus ullam quod ex modi 
+                                repudiandae, provident impedit ab amet culpa rerum aspernatur aut?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <div class="imgBx">
+                            <img src="http://placehold.it/275">
+                        </div>
+                        <div class="content">
+                            <div>
+                                <h2>Image Title</h2>
+                                <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur pariatur 
+                                nesciunt adipisci quam molestiae quasi quibusdam accusamus ullam quod ex modi 
+                                repudiandae, provident impedit ab amet culpa rerum aspernatur aut?
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="imgBx">
-                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/puits_une_01-1024x355.jpg">
-                    </div>
-                    <div class="content">
-                        <div>
-                            <h2>Image Title</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur pariatur 
-                                nesciunt adipisci quam molestiae quasi quibusdam accusamus ullam quod ex modi 
-                                repudiandae, provident impedit ab amet culpa rerum aspernatur aut?
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="imgBx">
-                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/lits_une-1024x355.jpg">
-                    </div>
-                    <div class="content">
-                        <div>
-                            <h2>Image Title</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur pariatur 
-                                nesciunt adipisci quam molestiae quasi quibusdam accusamus ullam quod ex modi 
-                                repudiandae, provident impedit ab amet culpa rerum aspernatur aut?
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="imgBx">
-                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/moussoro_une-1024x355.jpg">
-                    </div>
-                    <div class="content">
-                        <div>
-                            <h2>Image Title</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur pariatur 
-                                nesciunt adipisci quam molestiae quasi quibusdam accusamus ullam quod ex modi 
-                                repudiandae, provident impedit ab amet culpa rerum aspernatur aut?
-                            </p>
-                        </div>
-                    </div>
+                <div class="text-center mt-4">
+                    <a class="lien_projet" href="#">voir plus...</a>
                 </div>
             </div>
-
-            <div class="text-center mt-3">
-                <a class="lien_projet" href="#">voir plus...</a>
-            </div>
-
         </div>
 
-        <div class="bloc-4 justify-content-between">
-            <h2 class="titre">Nos partenaires</h2>
-            <div class="partenaire">
-                <div>
-                    <img src="http://pddt-tchad.org/wp-content/uploads/2016/04/logo_aerm.jpg" class="md:hover:bigger">
+        <div class="bloc-3">
+            <div class="container">
+                <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">Nos partenaires</h1>
+                <hr class="mt-2 mb-5">
+
+                <div class="partenaire justify-content-between">
+                    <div>
+                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/04/logo_aerm.jpg" class="md:hover:bigger">
+                    </div>
+                    <div>
+                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/Logo_la_guilde.gif">
+                    </div>
+                    <div>
+                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/Logo_entraide_solidarity.jpg">
+                    </div>
+                    <div>
+                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/Logo_ps_eau-300x300.png">
+                    </div>
                 </div>
-                <div>
-                    <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/Logo_la_guilde.gif">
-                </div>
-                <div>
-                    <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/Logo_entraide_solidarity.jpg">
-                </div>
-                <div>
-                    <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/Logo_ps_eau-300x300.png">
-                </div>
-                <div>
-                    <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/Logo_strasbourg-300x80.png">
-                </div>
-                <div>
-                    <img src="http://pddt-tchad.org/wp-content/uploads/2016/04/HUMANIS.png">
-                </div>
-                <div>
-                    <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/Logo_BSF-300x82.jpg">
-                </div>
-                <div>
-                    <img src="http://pddt-tchad.org/wp-content/uploads/2016/04/ecole_menage.png">
+                <div class="partenaire justify-content-between pt-5">
+                    <div>
+                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/Logo_strasbourg-300x80.png">
+                    </div>
+                    <div>
+                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/04/HUMANIS.png">
+                    </div>
+                    <div>
+                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/02/Logo_BSF-300x82.jpg">
+                    </div>
+                    <div>
+                        <img src="http://pddt-tchad.org/wp-content/uploads/2016/04/ecole_menage.png">
+                    </div>
                 </div>
             </div>
             
